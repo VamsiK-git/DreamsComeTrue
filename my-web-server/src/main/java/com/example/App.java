300834e9e906d54676971a71141d5a1d0f3a16aa
Vamsi Krishna <msdandapani@gmail.com>
2025-03-28 12:30:19 +0530

    Delete my-web-server directory

--- my-web-server/src/main/java/com/example/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // Initialize and start the web server
-        System.out.println("Starting the web server...");
-        // Add server initialization code here
-    }
-}
